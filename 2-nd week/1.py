38de55d521c06436e7247e0c7681a629dfe09d86
LevPogromov <lew.cherezow@gmail.com>
2024-09-27 23:24:55 +0300

    1-st solution

--- 2-nd week/1.py
+++ 2-nd week/1.py
@@ -3,22 +3,22 @@ https://leetcode.com/problems/count-and-say/description/
 ?envType=problem-list-v2&envId=string
 """
 
+
 class Solution:
     def countAndSay(self, n: int) -> str:
         if n == 1:
             return "1"
         ans = "1"
-        for i in range(2, n+1):
+        for i in range(2, n + 1):
             ans += "*"
             step = ""
             cnt = 1
-            for j in range(1, len(ans)): 
-                prev = ans[j-1]
-                if ans[j] == prev :
+            for j in range(1, len(ans)):
+                prev = ans[j - 1]
+                if ans[j] == prev:
                     cnt += 1
-                else: 
+                else:
                     step += str(cnt) + prev
                     cnt = 1
             ans = step
         return ans
-        
